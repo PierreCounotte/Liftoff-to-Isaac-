0b0aa0ad9161ed0ca093fc3de18f1e29b93223f2
PierreCounotte <pierre.counotte@student.uliege.be>
2025-08-04 17:08:13 +0200

    Rigid cube without any motion

--- LiftoffToIsaacTraj.py
+++ LiftoffToIsaacTraj.py
@@ -1,6 +1,97 @@
-import socket
-import struct
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""This script demonstrates how to spawn prims into the scene.
+
+.. code-block:: bash
+
+    # Usage
+    ./isaaclab.sh -p scripts/tutorials/00_sim/spawn_prims.py
+
+"""
+
+"""Launch Isaac Sim Simulator first."""
+
+
 import argparse
 
+from isaaclab.app import AppLauncher
 
-def 
+# create argparser
+parser = argparse.ArgumentParser(description="Tutorial on spawning prims into the scene.")
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import isaacsim.core.utils.prims as prim_utils
+
+import isaaclab.sim as sim_utils
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+
+
+CSV_PATH = "Big_trajectory_corrected.csv"  # ← à modifier
+CUBE_PRIM_PATH = "/World/Objects/Cube"
+FPS = 100  # fréquence d'échantillonnage du CSV (ex: 100 Hz → 0.01s entre lignes)
+
+
+def design_scene():
+    """Designs the scene by spawning ground plane, light, objects and meshes from usd files."""
+    # Ground-plane
+    cfg_ground = sim_utils.GroundPlaneCfg()
+    cfg_ground.func("/World/defaultGroundPlane", cfg_ground)
+
+    # spawn distant light
+    cfg_light_distant = sim_utils.DistantLightCfg(
+        intensity=3000.0,
+        color=(0.75, 0.75, 0.75),
+    )
+    cfg_light_distant.func(CUBE_PRIM_PATH, cfg_light_distant, translation=(1, 0, 10))
+
+    
+
+    # spawn a blue cuboid with deformable body
+    cfg_cuboid = sim_utils.MeshCuboidCfg(
+    size=(0.2, 0.2, 0.2),
+    rigid_props=sim_utils.RigidBodyPropertiesCfg(kinematic_enabled=True),                
+    collision_props=sim_utils.CollisionPropertiesCfg(),            
+    visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 0.0, 1.0)),
+)
+    cfg_cuboid.func("/World/Objects/CuboidRigid", cfg_cuboid, translation=(0.15, 0.0, 2.0))
+
+
+def main():
+    """Main function."""
+
+    # Initialize the simulation context
+    sim_cfg = sim_utils.SimulationCfg(dt=0.01, device=args_cli.device)
+    sim = sim_utils.SimulationContext(sim_cfg)
+    # Set main camera
+    sim.set_camera_view([2.0, 0.0, 2.5], [-0.5, 0.0, 0.5])
+
+    # Design scene by adding assets to it
+    design_scene()
+
+    # Play the simulator
+    sim.reset()
+    # Now we are ready!
+    print("[INFO]: Setup complete...")
+
+    # Simulate physics
+    while simulation_app.is_running():
+        # perform step
+        sim.step()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
