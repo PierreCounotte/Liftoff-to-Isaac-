a3f85f1053f5fc7b779046ec99e423a2ae7d0804
PierreCounotte <pierre.counotte@student.uliege.be>
2025-08-14 11:23:50 +0200

    Commented, functional code for running simulation either frome csv or udp sources

--- LiftoffToIsaacTraj.py
+++ LiftoffToIsaacTraj.py
@@ -1,20 +1,17 @@
+""" This script runs a drone trajectory in Isaac Lab using either a CSV file or UDP data stream."""
+
+
 import argparse
 from isaaclab.app import AppLauncher
 
-# create argparser
 parser = argparse.ArgumentParser(description="Run drone trajectory in Isaac Lab.")
-# append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
-# add custom argument
 parser.add_argument("--input_file", default="test.csv", help="CSV file containing the trajectory data")
 parser.add_argument("--data_source", choices=["csv", "udp"], help="Source of the trajectory data")
-# parse the arguments
 args_cli = parser.parse_args()
-# launch omniverse app
+
 app_launcher = AppLauncher(args_cli)
 simulation_app = app_launcher.app
-
-"""Rest everything follows."""
 
 import torch
 import pandas as pd
@@ -29,77 +26,81 @@ import keyboard
 import queue
 
 import isaacsim.core.utils.prims as prim_utils
-
 import isaaclab.sim as sim_utils
-import isaaclab.utils.math as math_utils
 from isaaclab.assets import RigidObject, RigidObjectCfg, Articulation, ArticulationCfg
 from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 from pxr import Usd
 
+""" Global variables for threading """
 recording = False
 terminate = False
 lock = threading.Lock()
-
 udp_stack = []
 
-
-sampling_frequency = 10000 # Hz
+""" Liftoff sampling frequency"""
+sampling_frequency = 100 # Hz
 
 
 def design_scene():
-    """Designs the scene by spawning ground plane, light, objects and meshes from usd files."""
-    # Ground-plane
+    """
+    Design the scene by adding assets to it. Creates a ground plane, a distant light,
+    and spawns a custom drone model.
+
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    scene_entities : dict
+        A dictionary containing the drone object.
+    """
+    
+    """ Ground plane and distant light"""
     cfg_ground = sim_utils.GroundPlaneCfg()
     cfg_ground.func("/World/defaultGroundPlane", cfg_ground)
-    # spawn distant light
     cfg_light_distant = sim_utils.DistantLightCfg(
         intensity=3000.0,
         color=(0.75, 0.75, 0.75),  
     )
     cfg_light_distant.func("/World/lightDistant", cfg_light_distant, translation=(1, 0, 10))
 
-    
-
-    # create a new xform prim for all objects to be spawned under
     prim_utils.create_prim("/World/Objects", "Xform")
 
-    """drone_cfg = RigidObjectCfg(
-        prim_path="/World/Objects/Vapor_X5",
-        spawn=sim_utils.UsdFileCfg(
-            #usd_path=f"{ISAAC_NUCLEUS_DIR}/Robots/Crazyflie/cf2x.usd",
-            usd_path = f"C:/Users/Administrateur/Documents/DroneProject/Liftoff-to-Isaac-/Vapor_X5/my_drone/my_drone.usd",
-            #rigid_props=sim_utils.RigidBodyPropertiesCfg(disable_gravity=True),
-        ),
-        init_state=RigidObjectCfg.InitialStateCfg()
-    )"""
-
-    
-
-
+    """ Drone object"""
     drone_cfg = ArticulationCfg(
         prim_path="/World/Objects/Vapor_X5",
         spawn=sim_utils.UsdFileCfg(
-            #usd_path=f"{ISAAC_NUCLEUS_DIR}/Robots/Crazyflie/cf2x.usd",
             usd_path = f"C:/Users/Administrateur/Documents/DroneProject/Liftoff-to-Isaac-/Vapor_X5_along_x/my_drone/my_drone.usd",
-            #rigid_props=sim_utils.RigidBodyPropertiesCfg(disable_gravity=True),
         ),
         init_state=ArticulationCfg.InitialStateCfg(),
         actuators={},  
     )
 
-    
-
-
     drone_object = Articulation(cfg=drone_cfg)
-    #drone_object = RigidObject(cfg=drone_cfg)
 
-    # return the scene information
     scene_entities = {"drone": drone_object}
     return scene_entities
 
 
 
 def LiftoffToIsaacCoordinates(df):
+    """
+    Converts the coordinates from Liftoff to Isaac conventions.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame containing the trajectory data with at least the following columns:
+        - position_x, position_y, position_z
+        - quaternion_x, quaternion_y, quaternion_z, quaternion_w
+        -velocity_x, velocity_y, velocity_z
+    
+    Returns
+    -------
+    None
+    Modifies the Dataframe in place to create a new one.
+    """
     x_unity = df['position_x'].copy()
     df.loc[:, 'position_x'] = df['position_z']
     df.loc[:, 'position_z'] = df['position_y']
@@ -111,39 +112,28 @@ def LiftoffToIsaacCoordinates(df):
     df.loc[:, 'velocity_y'] = -vx_unity
 
 
+    """ Unity format quaternions """
+    quat_unity = df[['quaternion_x', 'quaternion_y', 'quaternion_z', 'quaternion_w']].values
 
-
-    
-    # Unity format: (x, y, z, w)
-    quat_unity = df[['quaternion_x', 'quaternion_y', 'quaternion_z', 'quaternion_w']].values  # shape = (N, 4)
-
-    # Étape 1 : Conversion en rotation
+    """ Converts Unity format quaternions to Isaac format"""
     r_unity = R.from_quat(quat_unity)
-
     P = np.array([
         [0, 0, 1],
         [-1, 0, 0],
         [0, 1, 0]
     ])
-
-
     rot_mats = r_unity.as_matrix()
     rot_mats_converted = P @ rot_mats @ P.T
-
     angles = R.from_matrix(rot_mats_converted).as_euler('xyz', degrees=False)
-
-    # Swap roll et yaw
     angles[:, [0, 2]] = angles[:, [2, 0]]
     angles[:, 0] *= -1  # Roll
     angles[:, 2] *= -1  # Yaw
-    
 
     r_converted = R.from_euler('xyz', angles, degrees=False)
-
     r_z_180 = R.from_euler('x', -180, degrees=True)   
     r_final = r_z_180 * r_converted
 
-
+    """ Isaac format quaternions """
     quat_converted = r_final.as_quat()
     df['quaternion_x'] = quat_converted[:, 0]
     df['quaternion_y'] = quat_converted[:, 1]
@@ -153,19 +143,35 @@ def LiftoffToIsaacCoordinates(df):
 
 
 
-def run_simulator_from_csv(sim: sim_utils.SimulationContext, entities: dict[str, RigidObject], csv_path: str):
+def run_simulator_from_csv(sim: sim_utils.SimulationContext, entities: dict[str, Articulation], csv_path: str):
+    """
+    Run the simulation using a CSV file containing Liftoff trajectory data.
+
+    Parameters
+    ----------
+    sim : sim_utils.SimulationContext
+        The simulation context.
+    entities : dict[str, Articulation]
+        Dictionary containing the drone object.
+    csv_path : str
+        Path to the CSV file containing the trajectory data.
+    
+    Returns
+    -------
+    None  
+    """
     drone_object = entities["drone"]
     sim_dt = sim.get_physics_dt()
 
     if not os.path.exists(csv_path):
         print(f"{csv_path} does not exist.")
         return
+    
+    """ Read CSV file """
     row_stream = live_csv_reader(csv_path)
 
     sim_time = 0.0
     count = 0
-
-
     while simulation_app.is_running():
         try:
             row = next(row_stream)
@@ -173,39 +179,33 @@ def run_simulator_from_csv(sim: sim_utils.SimulationContext, entities: dict[str,
             print("[INFO] Data flux end.")
             return
 
-
-        x_pos = row['position_x']-1000
-        y_pos = row['position_y']+1070
+        """ Recover drone position and orientation from the DataFrame row"""
+        x_pos = row['position_x']-1000 # Center drone in the map
+        y_pos = row['position_y']+1070 # Center drone in the map
         z_pos = row['position_z']
         qx = row['quaternion_x']
         qy = row['quaternion_y']
         qz = row['quaternion_z']
         qw = row['quaternion_w']
 
+        """ Write the drone state into the simulation"""
         root_state = drone_object.data.default_root_state.clone()
         root_state[:, :3] = torch.tensor([x_pos, y_pos, z_pos], device=root_state.device)
         root_state[:, 3:7] = torch.tensor([qx, qy, qz, qw], device=root_state.device)
-
         drone_object.write_root_pose_to_sim(root_state[:, :7])
         drone_object.write_root_velocity_to_sim(root_state[:, 7:])
-
         drone_object.reset()
         drone_object.write_data_to_sim()
+
         sim.step()
 
         drone_object.update(sim_dt)
-
                 
-        
+        """ Update camera position and target to follow the drone """
         drone_pose = drone_object.data.root_state_w.clone() 
-        drone_pose_np = drone_pose.cpu().numpy()     # shape: (1, 13)
-        drone_pos = drone_pose_np[0, :3]              # (x, y, z)
-        drone_rot = drone_pose_np[0, 3:7]   
-
-
-        
+        drone_pose_np = drone_pose.cpu().numpy()     
+        drone_pos = drone_pose_np[0, :3]           
         offset_local = np.array([-1.5, 0.0, 1.5])  
-
 
         camera_position = drone_pos + offset_local 
         camera_target = drone_pos
@@ -216,12 +216,7 @@ def run_simulator_from_csv(sim: sim_utils.SimulationContext, entities: dict[str,
         if count % 50 == 0:
             print(f"Timestamp : {sim_time:.2f} s, Root position (in world): {drone_object.data.root_pos_w}")
             
-        if count % 1000 == 0 and count != 0:
-            print(f"[INFO] --- Resetting sim_time at count = {count} ---")
-            sim_time = 0.0
-            print(f"Timestamp : {sim_time:.2f} s, Root position (in world): {drone_object.data.root_pos_w}")
         count += 1
-
         sim_time += sim_dt
     
 
@@ -229,6 +224,24 @@ def run_simulator_from_csv(sim: sim_utils.SimulationContext, entities: dict[str,
 
 
 def live_csv_reader(file_path, start_row=0, sleep_time=0.1, timeout=30.0):
+    """
+    Reads a CSV file in real-time, yielding new rows as they are added.
+
+    Parameters
+    ----------
+    file_path : str
+        Path to the CSV file.
+    start_row : int, optional
+        Row number to start reading from (default is 0).
+    sleep_time : float, optional
+        Time to wait between checks for new data (default is 0.1 seconds).
+    timeout : float, optional
+        Time to wait before stopping if no new data is received (default is 30 seconds).
+    Returns
+    -------
+    generator
+        A generator that yields new rows as they are added to the CSV file. 
+    """
     last_row_read = start_row
     waited_time = 0.0
     print(f"[INFO] Starting to read from {file_path}...")
@@ -257,6 +270,17 @@ def live_csv_reader(file_path, start_row=0, sleep_time=0.1, timeout=30.0):
 
 
 def toggle_recording_keypress():
+    """
+    Toggles recording state on keypress ('a').
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
     global recording, terminate
     print("Press 'a' to start / end recording.")
     while True:
@@ -270,33 +294,59 @@ def toggle_recording_keypress():
                 terminate = True
                 break  
 
-# --- Thread : lit l'UDP et stocke les lignes ---
+
 def udp_listener(port):
+    """
+    Listens for UDP packets and appends new rows to the udp_stack.
+    Add new rows to the udp_stack.
+
+    Parameters
+    ----------
+    port : int
+        The UDP port to listen on.
+
+    Returns
+    -------
+    None
+    """
     for row in listen_trajectory(port):
-        # Remplace complètement l'élément précédent
         if udp_stack:
             udp_stack.pop()
         udp_stack.append(row)
 
-# --- Boucle principale : lit la queue et met à jour la simulation ---
-def run_simulator_from_udp(sim: sim_utils.SimulationContext, entities: dict[str, RigidObject], port):
+
+def run_simulator_from_udp(sim: sim_utils.SimulationContext, entities: dict[str, Articulation], port):
+    """
+    Run the simulation using UDP data stream from Liftoff.
+
+    Parameters
+    ----------
+    sim : sim_utils.SimulationContext
+        The simulation context.
+    entities : dict[str, Articulation]
+        Dictionary containing the drone object.
+    port : int
+        The UDP port to listen on for trajectory data.
+    
+    Returns
+    -------
+    None
+    """
     drone_object = entities["drone"]
     sim_dt = sim.get_physics_dt()
 
-    # Lancer l'écoute UDP dans un thread séparé
+    """ Start the UDP listener in a separate thread """
     threading.Thread(target=udp_listener, args=(port,), daemon=True).start()
 
     sim_time = 0.0
     count = 0
-
     while simulation_app.is_running():
         row = None
         if recording and udp_stack:
-            row = udp_stack.pop()  # Prend la dernière position
-        # Ne garde rien d'autre dans la stack pour éviter le retard
+            row = udp_stack.pop()  
         udp_stack.clear()
 
-        # --- Mise à jour UDP et drone seulement si recording ---
+        """ If recording is enabled and a new row is available, update the drone state."""
         if recording and row is not None:
 
             x_pos = row['position_x'] - 1000
@@ -321,10 +371,9 @@ def run_simulator_from_udp(sim: sim_utils.SimulationContext, entities: dict[str,
             if count % 50 == 0:
                 print(f"Timestamp : {sim_time:.2f} s, Root position (in world): {drone_object.data.root_pos_w}")
 
-        # --- Faire avancer la simulation toujours ---
         sim.step()
 
-        # --- Mise à jour caméra toujours ---
+        """ Update camera position and target to follow the drone """
         drone_pose = drone_object.data.root_state_w.clone()
         drone_pose_np = drone_pose.cpu().numpy()
         drone_pos = drone_pose_np[0, :3]
@@ -340,14 +389,25 @@ def run_simulator_from_udp(sim: sim_utils.SimulationContext, entities: dict[str,
 
 
 def listen_trajectory(port):
-    #global recording, terminate
+    """
+    Listens for UDP packets containing drone trajectory data and yields rows of data.
+
+    Parameters
+    ----------
+    port : int
+        The UDP port to listen on for trajectory data.
+    
+    Returns
+    -------
+    generator
+        A generator that yields rows of trajectory data as pandas Series.
+    """
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
         udp_socket.bind(('0.0.0.0', port))
         print(f"Listening for UDP packets on port {port}...")
 
         df = pd.DataFrame()
-
 
         while not terminate:
             try:
@@ -400,23 +460,34 @@ def listen_trajectory(port):
             except Exception as e:
                 print(f"Parsing error: {e}")
 
-        # À la fin, fermer le fichier CSV s'il est ouvert
     
 
 def main(args_cli):
-    """Main function."""
+    """
+    Main function to run the drone trajectory in Isaac Lab.
+
+    Parameters
+    ----------
+    args_cli : argparse.Namespace
+        Command line arguments parsed by argparse.
     
-    # Initialize the simulation context
+    Returns
+    -------
+    None
+    """
+    
+    """ Initialize the simulation application and set up the simulation context. """
     sim_cfg = sim_utils.SimulationCfg(dt=1/sampling_frequency, device=args_cli.device)
     sim = sim_utils.SimulationContext(sim_cfg)
-    # Set main camera
+    simulation_app.set_setting("/app/asyncRendering", True)
     sim.set_camera_view([-2, 0, 4], [3, 0.0, 0.0])
-    # Design scene by adding assets to it
+
     scene_entities = design_scene()
-    # Play the simulator
     sim.reset()
-    # Now we are ready!
+
     print("[INFO]: Setup complete...")
+
+    """ Run the simulation based on the data source specified in the command line arguments. """
 
     if args_cli.data_source == "csv":
         csv_path = args_cli.input_file
@@ -437,8 +508,7 @@ def main(args_cli):
 
 
 if __name__ == "__main__":
-    # run the main function
-    main(args_cli)
 
+    main(args_cli)
     simulation_app.close()
     
